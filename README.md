c21476065670acb3942d28b0017fdd987aae24bc
hysong <hachu.developer@gmail.com>
2021-12-16 12:00:08 +0900

    react-chartjs-2 라이브러리 추가

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 1. [ chart js ](https://www.chartjs.org/docs/latest/)
 2. [ react-query ](https://react-query.tanstack.com/)
 3. [ flexbox ](https://d2.naver.com/helloworld/8540176)
+4. [ Vercel ](https://velog.io/@anjoy/%EB%B8%94%EB%A1%9C%EA%B7%B8%EB%A7%8C%EB%93%A4%EA%B8%B011-Vercel%EB%A1%9C-%ED%94%84%EB%A1%A0%ED%8A%B8%EB%8B%A8-%EB%B0%B0%ED%8F%AC%ED%95%98%EA%B8%B0)
--- pages/monitoring.tsx
+++ pages/monitoring.tsx
@@ -1,9 +1,31 @@
-import { NextPage } from 'next';
+import { NextPage } from "next";
+import { Line } from "react-chartjs-2";
+
+
 
 const Monitoring: NextPage = () => {
   return (
-    <div>여기는 Monitoring 화면</div>
-  )
-}
+    <>
+      <div>여기는 Monitoring 화면</div>
+      <Line
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+            {
+              label: "첫번째",
+              data: [5, 6, 7, 0, 1, 2, 3, 4],
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255, 99, 132, 1)",
+            },
+            {
+              label: "두번째",
+              data: [3, 2, 1, 9, 0, 8, 1, 2],
+            },
+          ],
+        }}
+      />
+    </>
+  );
+};
 
 export default Monitoring;
